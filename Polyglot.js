250eb6a8d4862d5ee0c00eb32ee3116bd1795aa3
ForgotMyCode <nespamujtady@gmail.com>
2021-05-28 21:15:16 +0200

    The polyglot

---
+++ Polyglot.js
@@ -0,0 +1,26 @@
+/*
+\u002a\u002f
+class Polyglot
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World!");
+    }
+}
+\u002f\u002a
+*/
+// \u000a\u002f\u002a
+// \
+/*
+#include <stdio.h>
+int main() {
+    printf("Hello, World!");
+}
+void print(char* x) {}
+void f() 
+// \
+*/ // \u000a\u002f\u002a
+{
+print("Hello, World!");
+}
+// \u000a\u002a\u002f
